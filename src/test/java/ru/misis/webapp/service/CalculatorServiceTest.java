a78115c0ef25b774c422516f1e278cbfdd219e36
vacationl33t <66736174+vacationl33t@users.noreply.github.com>
2024-01-12 11:01:43 +0300

    Update CalculatorServiceTest.java

--- src/test/java/ru/misis/webapp/service/CalculatorServiceTest.java
+++ src/test/java/ru/misis/webapp/service/CalculatorServiceTest.java
@@ -7,17 +7,10 @@ import ru.misis.webapp.dto.ResultDto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-/**
- * Тесты для {@link CalculatorService}
- *
- * @author Alexander Dedov
- * @since 31.12.2023
- */
+
 class CalculatorServiceTest {
 
-    /**
-     * Тестирование сценария сложения.
-     */
+
     @Test
     void calculateAddition() {
         CalculatorDataDto calculatorData = new CalculatorDataDto(5.0, "ADD", 3.0);
@@ -27,9 +20,7 @@ class CalculatorServiceTest {
         assertEquals(8.0, resultDto.getResult());
     }
 
-    /**
-     * Тестирование сценария вычитания.
-     */
+
     @Test
     void calculateSubtraction() {
         CalculatorDataDto calculatorData = new CalculatorDataDto(8.0, "SUBTRACT", 3.0);
@@ -39,9 +30,7 @@ class CalculatorServiceTest {
         assertEquals(5.0, resultDto.getResult());
     }
 
-    /**
-     * Тестирование сценария умножения.
-     */
+
     @Test
     void calculateMultiplication() {
         CalculatorDataDto calculatorData = new CalculatorDataDto(4.0, "MULTIPLY", 2.0);
@@ -51,9 +40,7 @@ class CalculatorServiceTest {
         assertEquals(8.0, resultDto.getResult());
     }
 
-    /**
-     * Тестирование сценария деления.
-     */
+
     @Test
     void calculateDivision() {
         CalculatorDataDto calculatorData = new CalculatorDataDto(9.0, "DIVIDE", 3.0);
@@ -63,9 +50,7 @@ class CalculatorServiceTest {
         assertEquals(3.0, resultDto.getResult());
     }
 
-    /**
-     * Тестирование сценария с неизвестной операцией.
-     */
+
     @Test
     void calculateUnknownOperation() {
         CalculatorDataDto calculatorData = new CalculatorDataDto(5.0, "UNKNOWN", 3.0);
