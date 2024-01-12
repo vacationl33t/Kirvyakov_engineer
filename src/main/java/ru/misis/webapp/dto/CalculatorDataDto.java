ada0fbaa5fcfac47e7b24bef136545ae51b31a3d
vacationl33t <66736174+vacationl33t@users.noreply.github.com>
2024-01-12 11:00:00 +0300

    Update CalculatorDataDto.java

--- src/main/java/ru/misis/webapp/dto/CalculatorDataDto.java
+++ src/main/java/ru/misis/webapp/dto/CalculatorDataDto.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * DTO с полями для калькуляции
- *
- * @author Alexander Dedov
- * @since 28.12.2023
- */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
