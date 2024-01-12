a4f2fa3c81d486d14fe7d0b192386f89648ab426
vacationl33t <66736174+vacationl33t@users.noreply.github.com>
2024-01-12 11:00:12 +0300

    Update ResultDto.java

--- src/main/java/ru/misis/webapp/dto/ResultDto.java
+++ src/main/java/ru/misis/webapp/dto/ResultDto.java
@@ -3,12 +3,7 @@ package ru.misis.webapp.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/**
- * DTO результата вычислений
- *
- * @author Alexander Dedov
- * @since 28.12.2023
- */
+
 @Data
 @AllArgsConstructor
 public class ResultDto {
